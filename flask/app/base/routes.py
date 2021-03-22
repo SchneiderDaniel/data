73524c1babd252cb0210806f8ed8153a4dab5af3
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-03-22 19:03:11 +0100

    flash added

--- flask/app/base/routes.py
+++ flask/app/base/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, render_template, redirect, request, url_for
+from flask import jsonify, render_template, redirect, request, url_for,flash
 from . import blueprint
 
 
--- flask/app/home/routes.py
+++ flask/app/home/routes.py
@@ -1,7 +1,8 @@
 from . import blueprint
-from flask import render_template
+from flask import render_template, flash
 
 
 @blueprint.route('/')
 def index():
+    flash('Important: Note, this site is still in preview!', 'warning')
     return render_template('index.html')
