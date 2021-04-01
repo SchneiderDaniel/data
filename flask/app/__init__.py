f3d3f19295a3227f26135d9c542bc0af2a7bc147
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-04-01 20:36:33 +0200

    finance blueprint

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -36,7 +36,7 @@ def init_admin(app):
     admin.add_view(MyAdminView(Role, db_session))
 
 def register_blueprints(app):
-    for module_name in ('base', 'home', 'datamath','tools', 'contact'):
+    for module_name in ('base', 'home', 'datamath','finance', 'contact'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
--- flask/app/finance/__init__.py
+++ flask/app/finance/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 
 blueprint = Blueprint(
-    'DashExample_blueprint',
+    'finance_blueprint',
     __name__,
-    url_prefix='/tools',
+    url_prefix='/finance',
     template_folder='templates',
     static_folder='static'
 )
