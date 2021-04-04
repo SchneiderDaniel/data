cda99c340ec9709955016efe839259344f2e7bbc
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-04-04 21:14:14 +0200

    styles

--- flask/app/home/routes.py
+++ flask/app/home/routes.py
@@ -4,5 +4,5 @@ from flask import render_template, flash
 
 @blueprint.route('/')
 def index():
-    flash('Important: Note, this site is still in preview!', 'warning')
+    # flash('Important: Note, this site is still in preview!', 'warning')
     return render_template('index.html')
